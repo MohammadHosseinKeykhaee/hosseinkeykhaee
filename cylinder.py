c8dde4fa7350a0fd1321ae88c721fcfb7df57f7c
Hossein <110756265+hosseinkeykhaee@users.noreply.github.com>
2022-08-07 13:31:44 +0430

    Add files via upload

---
+++ cylinder.py
@@ -0,0 +1,9 @@
+radius = float(input("please enter the radius of the cylinder :"))
+height= float(input("please enter the length height of the cylinder:"))
+pi = 3.1415
+lateral_Area = 2*pi*radius*height
+cylinder_volume = pi*(radius**2)*(height)
+Total_Area = lateral_Area + (2*pi*radius**2)
+print ("the lateral area of the cylinder is", lateral_Area, "cm^2" )
+print ("the Valume of the cylinder is", cylinder_volume , "cm^3" )
+print ("the Total area of the cylinder is", Total_Area , "cm^2" )
