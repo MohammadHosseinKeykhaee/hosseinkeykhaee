53dd21efae6e0d1bb85c65f009045cf821f3f8ff
Mohammad Hossein Keykhaee <110756265+MohammadHosseinKeykhaee@users.noreply.github.com>
2022-08-08 16:31:50 +0430

    Delete Area_of_ the_ triangle_edit.py

--- Area_of_ the_ triangle_edit.py
+++
@@ -1,4 +0,0 @@
-base = float(input("please enter the length base of the triangle:"))
-height = float(input("please enter the length height of the triangle:"))
-Area = 0.5*base*height
-print ("the area of the triangle is", Area )
